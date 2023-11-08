4a2f5d711551836fa0009bd76b93d647df91a237
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-11-08 22:37:13 +0700

    Init server

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT ?? 8888;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+    console.log('JSON Server is running on:', PORT);
+});
